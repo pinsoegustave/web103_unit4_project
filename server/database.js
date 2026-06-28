6411e53023c8e48a72939587a1946d9cc2c2d735
pinsoegustave <ntwalimudasubirag@gmail.com>
2026-06-28 16:23:13 -0400

    add db connections

--- server/database.js
+++ server/database.js
@@ -4,8 +4,12 @@ import './dotenv.js';
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE,
+    ssl: { rejectUnauthorized: false }
 });
 
 export default pool;
